c96995cac93726a7d0adf0b809c244f3a854a1dc
jerpelea <jerpelea@gmail.com>
2011-09-27 15:52:16 +0300

    semc: .sh and recovery update
    Change-Id: I971aa04a361ce29d9b7f87f81e36511727d297fd

--- device_urushi.mk
+++ device_urushi.mk
@@ -42,21 +42,6 @@ PRODUCT_COPY_FILES += \
     device/semc/urushi/prebuilt/bootrec:root/sbin/bootrec \
     device/semc/urushi/recovery.fstab:root/recovery.fstab
 
-#recovery resources
-PRODUCT_COPY_FILES += \
-    bootable/recovery/res/images/icon_clockwork.png:root/res/images/icon_clockwork.png \
-    bootable/recovery/res/images/icon_error.png:root/res/images/icon_error.png \
-    bootable/recovery/res/images/icon_installing.png:root/res/images/icon_installing.png \
-    bootable/recovery/res/images/indeterminate1.png:root/res/images/indeterminate1.png \
-    bootable/recovery/res/images/indeterminate2.png:root/res/images/indeterminate2.png \
-    bootable/recovery/res/images/indeterminate3.png:root/res/images/indeterminate3.png \
-    bootable/recovery/res/images/indeterminate4.png:root/res/images/indeterminate4.png \
-    bootable/recovery/res/images/indeterminate5.png:root/res/images/indeterminate5.png \
-    bootable/recovery/res/images/indeterminate6.png:root/res/images/indeterminate6.png \
-    bootable/recovery/res/images/progress_empty.png:root/res/images/progress_empty.png \
-    bootable/recovery/res/images/progress_fill.png:root/res/images/progress_fill.png
-
-
 #WIFI modules and configs
 PRODUCT_COPY_FILES += \
     device/semc/msm7x30-common/prebuilt/tiap_loader.sh:system/bin/tiap_loader.sh \
