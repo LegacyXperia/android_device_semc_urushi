54d6537cb8ae00da8266cac608db4f5f9166aa66
freexperia <jerpelea@gmail.com>
2011-12-22 11:53:08 +0200

    mogami:update for 4.0.3 and new 4.1 kernel

--- cm.mk
+++ cm.mk
@@ -1,13 +1,20 @@
-$(call inherit-product, device/semc/urushi/device_urushi.mk)
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := urushi
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
+# Inherit device configuration
+$(call inherit-product, device/semc/urushi/device_urushi.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=ST18i BUILD_ID=4.0.A.2.368 BUILD_FINGERPRINT=SEMC/ST18i_1247-1061/ST18i:2.3.3/3.0.1.A.0.145/bn_P:user/release-keys PRIVATE_BUILD_DESC="ST18i-user 2.3.3 3.0.1.A.0.145 bn_P test-keys"
-
-PRODUCT_NAME := cm_urushi
+## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := urushi
+PRODUCT_NAME := cm_urushi
+PRODUCT_BRAND := SEMC
+PRODUCT_MODEL := Xperia Ray
 
+#Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=urushi BUILD_ID=ITL41F BUILD_DISPLAY_ID=ITL41F BUILD_FINGERPRINT="google/yakju/maguro:4.0.1/ITL41F/228551:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.0.1 ITL41F 228551 release-keys"
