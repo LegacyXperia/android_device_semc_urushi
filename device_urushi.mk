475298c16f0d0332889236b9baedb6611b159495
freexperia <jerpelea@gmail.com>
2012-01-25 16:11:41 +0200

    semc: updated kernel and cleanup

--- device_urushi.mk
+++ device_urushi.mk
@@ -11,18 +11,7 @@ PRODUCT_NAME := urushi
 PRODUCT_DEVICE := urushi
 PRODUCT_MODEL := urushi
 
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-LOCAL_KERNEL := device/semc/urushi/kernel
-else
-LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
 -include device/semc/mogami-common/mogami.mk
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
-
-DISABLE_DEXPREOPT := false
 
 # These is the hardware-specific overlay, which points to the location
 # of hardware-specific resource overrides, typically the frameworks and
