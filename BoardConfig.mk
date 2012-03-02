64de079856210db94449a35cf7c05dae6fd8cfcd
freexperia <jerpelea@gmail.com>
2012-03-02 04:14:39 +0200

    semc: enables sensor build and prop cleanup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,13 +1,10 @@
 -include device/semc/mogami-common/BoardConfigCommon.mk
 -include vendor/semc/urushi/BoardConfigVendor.mk
 
-BUILD SEMC SENSORS := false
 SENSORS_COMPASS_AK897X := true
 SENSORS_ACCEL_BMA150_INPUT := true
 SENSORS_ACCEL_BMA250_INPUT := false
 SENSORS_PROXIMITY_APDS970X := true
 SENSORS_PRESSURE_BMP180 := false
-
-#BOARD_FLASH_BLOCK_SIZE := 4096
 
 TARGET_OTA_ASSERT_DEVICE := ST18i,ST18a,urushi
