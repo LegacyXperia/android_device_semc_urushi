4b63f6e5b1db58dbab72c84328cbd1e07d6f90ba
Diogo Ferreira <defer@cyanogenmod.com>
2012-03-10 17:59:23 +0000

    urushi: Copy dpi-dependent resources using a generic makefile from msm7x30-common
    Change-Id: Ia17c63a6cdb49dbe313482742b71802109403863

--- device_urushi.mk
+++ device_urushi.mk
@@ -38,16 +38,7 @@ PRODUCT_COPY_FILES += \
 # semc msm7x30 uses high-density artwork where available
 PRODUCT_LOCALES += hdpi
 
-#Offline charging animation
-PRODUCT_COPY_FILES += \
-    device/semc/msm7x30-common/prebuilt/animations/charging_animation_01_H.png:system/semc/chargemon/data/charging_animation_01.png \
-    device/semc/msm7x30-common/prebuilt/animations/charging_animation_02_H.png:system/semc/chargemon/data/charging_animation_02.png \
-    device/semc/msm7x30-common/prebuilt/animations/charging_animation_03_H.png:system/semc/chargemon/data/charging_animation_03.png \
-    device/semc/msm7x30-common/prebuilt/animations/charging_animation_04_H.png:system/semc/chargemon/data/charging_animation_04.png \
-    device/semc/msm7x30-common/prebuilt/animations/charging_animation_05_H.png:system/semc/chargemon/data/charging_animation_05.png \
-    device/semc/msm7x30-common/prebuilt/animations/charging_animation_06_H.png:system/semc/chargemon/data/charging_animation_06.png \
-    device/semc/msm7x30-common/prebuilt/animations/charging_animation_07_H.png:system/semc/chargemon/data/charging_animation_07.png
-#    device/semc/msm7x30-common/prebuilt/animations/charging_animation_blank_H.png:system/semc/chargemon/data/charging_animation_blank.png
+-include device/semc/msm7x30-common/prebuilt/resources-hdpi.mk
 
 #LCD DENSITY
 PRODUCT_PROPERTY_OVERRIDES += \
