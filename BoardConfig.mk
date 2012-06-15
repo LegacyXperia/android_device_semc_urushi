0ce9194bbf06dfb10e1472fd5479761c4096bfcf
freexperia <jerpelea@gmail.com>
2012-06-15 17:34:19 +0300

    urushi: Enable integrated kernel building

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,6 +1,10 @@
 -include device/semc/mogami-common/BoardConfigCommon.mk
 -include vendor/semc/urushi/BoardConfigVendor.mk
 
+TARGET_KERNEL_SOURCE := kernel/semc/msm7x30
+TARGET_KERNEL_CONFIG := cyanogen_urushi_defconfig
+TARGET_PREBUILT_KERNEL := device/semc/urushi/prebuilt/kernel
+
 SENSORS_COMPASS_AK897X := true
 SENSORS_ACCEL_BMA150_INPUT := true
 SENSORS_ACCEL_BMA250_INPUT := false
--- device_urushi.mk
+++ device_urushi.mk
@@ -11,10 +11,6 @@ PRODUCT_NAME := urushi
 PRODUCT_DEVICE := urushi
 PRODUCT_MODEL := urushi
 
-TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
-PRODUCT_COPY_FILES += \
-    $(TARGET_PREBUILT_KERNEL):kernel
-
 -include device/semc/mogami-common/mogami.mk
 
 # These is the hardware-specific overlay, which points to the location
