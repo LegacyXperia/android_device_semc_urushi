82691937cb003b2714b4319a5237688f4ad7f7ed
freexperia <jerpelea@gmail.com>
2012-09-08 07:53:19 +0300

    usurhi: bootanimation

--- cm.mk
+++ cm.mk
@@ -10,7 +10,8 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 #Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=MT11i BUILD_FINGERPRINT="SEMC/MT11i_1254-2184/MT11i:4.0.4/4.1.B.0.431/UL5_3w:user/release-keys" PRIVATE_BUILD_DESC="MT11i-user 4.0.4 4.1.B.0.431 UL5_3w test-keys"
 
-TARGET_BOOTANIMATION_NAME := vertical-480x854
+TARGET_SCREEN_HEIGHT := 854
+TARGET_SCREEN_WIDTH := 480
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := cm_urushi
