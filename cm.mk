0e3932ffecffdaa63f5da87c84b883fe70a87381
Michael Bestas <mikeioannina@gmail.com>
2013-04-15 18:04:45 +0300

    urushi: update build fingerprint
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: Ie972f9e0bcbfa3f9e700c744d6a13162b0da6dcb

--- cm.mk
+++ cm.mk
@@ -8,7 +8,7 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=MT11i BUILD_FINGERPRINT="SEMC/ST18i_1254-2184/ST18i:4.0.4/4.1.B.0.431/UL5_3w:user/release-keys" PRIVATE_BUILD_DESC="ST18i-user 4.0.4 4.1.B.0.431 UL5_3w test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=ST18i BUILD_FINGERPRINT="SEMC/ST18i_1254-2184/ST18i:4.0.4/4.1.B.0.587/tL1_3w:user/release-keys" PRIVATE_BUILD_DESC="ST18i-user 4.0.4 4.1.B.0.587 tL1_3w test-keys"
 
 # Boot Animation
 TARGET_SCREEN_HEIGHT := 854
