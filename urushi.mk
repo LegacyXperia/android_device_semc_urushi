870e08cf04a05f2070f957fb26326935207ec95a
Michael Bestas <mikeioannina@gmail.com>
2013-04-30 04:40:40 +0300

    urushi: remove bootrec-device & obsolete als_curve
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: I123ae25cd5f3efa13fff9dc58525bf20433163c4

--- urushi.mk
+++ urushi.mk
@@ -20,10 +20,6 @@ PRODUCT_COPY_FILES += \
     device/semc/urushi/prebuilt/hw_config.sh:system/etc/hw_config.sh \
     device/semc/msm7x30-common/prebuilt/logo_H.rle:root/initlogo.rle
 
-# Device specific part for two-stage boot
-PRODUCT_COPY_FILES += \
-    device/semc/urushi/recovery/bootrec-device:recovery/bootrec-device
-
 # Device specific configs
 PRODUCT_COPY_FILES += \
     device/semc/urushi/config/clearpad.idc:system/usr/idc/clearpad.idc \
@@ -32,7 +28,6 @@ PRODUCT_COPY_FILES += \
     device/semc/urushi/config/msm_pmic_pwr_key.kl:system/usr/keylayout/msm_pmic_pwr_key.kl \
     device/semc/urushi/config/pm8058-keypad.kl:system/usr/keylayout/pm8058-keypad.kl \
     device/semc/urushi/config/simple_remote.kl:system/usr/keylayout/simple_remote.kl \
-    device/semc/urushi/config/als_curve.conf:system/etc/als_curve.conf \
     device/semc/urushi/config/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
     device/semc/urushi/config/sensors.conf:system/etc/sensors.conf \
     device/semc/urushi/config/tiwlan.ini:system/etc/tiwlan.ini
