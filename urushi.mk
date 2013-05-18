1eb66a9cbf963c25a131dfcf54110f1feccf9ec7
Michael Bestas <mikeioannina@gmail.com>
2013-05-18 16:55:04 +0300

    urushi: Initial reorganization of files
    Move files around like newer sony phones
    
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: Ic3f9eb73f60192b99c0732189d5501c17f2258a5

--- urushi.mk
+++ urushi.mk
@@ -16,23 +16,20 @@ PRODUCT_AAPT_PREF_CONFIG := hdpi
 
 # Init files
 PRODUCT_COPY_FILES += \
-    device/semc/urushi/prebuilt/init.semc.usb.rc:root/init.semc.usb.rc \
-    device/semc/urushi/prebuilt/hw_config.sh:system/etc/hw_config.sh \
-    device/semc/msm7x30-common/prebuilt/logo_H.rle:root/initlogo.rle
+    $(LOCAL_PATH)/rootdir/init.semc.usb.rc:root/init.semc.usb.rc \
+    $(LOCAL_PATH)/rootdir/system/etc/hw_config.sh:system/etc/hw_config.sh
 
 # Device specific configs
 PRODUCT_COPY_FILES += \
-    device/semc/urushi/config/clearpad.idc:system/usr/idc/clearpad.idc \
-    device/semc/urushi/config/atdaemon.kl:system/usr/keylayout/atdaemon.kl \
-    device/semc/urushi/config/clearpad.kl:system/usr/keylayout/clearpad.kl \
-    device/semc/urushi/config/msm_pmic_pwr_key.kl:system/usr/keylayout/msm_pmic_pwr_key.kl \
-    device/semc/urushi/config/pm8058-keypad.kl:system/usr/keylayout/pm8058-keypad.kl \
-    device/semc/urushi/config/simple_remote.kl:system/usr/keylayout/simple_remote.kl \
-    device/semc/urushi/config/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
-    device/semc/urushi/config/sensors.conf:system/etc/sensors.conf \
-    device/semc/urushi/config/tiwlan.ini:system/etc/tiwlan.ini
-
-$(call inherit-product, device/semc/msm7x30-common/prebuilt/resources-hdpi.mk)
+    $(LOCAL_PATH)/rootdir/system/usr/idc/clearpad.idc:system/usr/idc/clearpad.idc \
+    $(LOCAL_PATH)/rootdir/system/usr/keylayout/atdaemon.kl:system/usr/keylayout/atdaemon.kl \
+    $(LOCAL_PATH)/rootdir/system/usr/keylayout/clearpad.kl:system/usr/keylayout/clearpad.kl \
+    $(LOCAL_PATH)/rootdir/system/usr/keylayout/msm_pmic_pwr_key.kl:system/usr/keylayout/msm_pmic_pwr_key.kl \
+    $(LOCAL_PATH)/rootdir/system/usr/keylayout/pm8058-keypad.kl:system/usr/keylayout/pm8058-keypad.kl \
+    $(LOCAL_PATH)/rootdir/system/usr/keylayout/simple_remote.kl:system/usr/keylayout/simple_remote.kl \
+    $(LOCAL_PATH)/rootdir/system/etc/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
+    $(LOCAL_PATH)/rootdir/system/etc/sensors.conf:system/etc/sensors.conf \
+    $(LOCAL_PATH)/rootdir/system/etc/tiwlan.ini:system/etc/tiwlan.ini
 
 # Device properties
 PRODUCT_PROPERTY_OVERRIDES += \
