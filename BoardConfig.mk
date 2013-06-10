94d1685a2d2a27585d37f58cd6bae0e2ddb526c8
Michael Bestas <mikeioannina@gmail.com>
2013-06-10 21:29:40 +0300

    urushi: little cleanup
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: If2ea77c2b71f327690db208849f2ffa6d2d5184f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,6 +15,13 @@
 -include device/semc/mogami-common/BoardConfigCommon.mk
 -include vendor/semc/urushi/BoardConfigVendor.mk
 
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/semc/urushi/bluedroid
+
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 854
+TARGET_SCREEN_WIDTH := 480
+
 # Kernel
 TARGET_KERNEL_CONFIG := nAa_urushi_defconfig
 
@@ -22,10 +29,8 @@ TARGET_KERNEL_CONFIG := nAa_urushi_defconfig
 SOMC_CFG_SENSORS_ACCEL_BMA150_INPUT := yes
 SOMC_CFG_SENSORS_PROXIMITY_APDS9700 := yes
 
+# Assert
 TARGET_OTA_ASSERT_DEVICE := ST18i,ST18a,urushi
 
 # Torch
 COMMON_GLOBAL_CFLAGS += -DUSES_AS3676_TORCH
-
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/semc/urushi/bluedroid
--- cm.mk
+++ cm.mk
@@ -24,10 +24,6 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=ST18i BUILD_FINGERPRINT="SEMC/ST18i_1254-2184/ST18i:4.0.4/4.1.B.0.587/tL1_3w:user/release-keys" PRIVATE_BUILD_DESC="ST18i-user 4.0.4 4.1.B.0.587 tL1_3w test-keys"
 
-# Boot Animation
-TARGET_SCREEN_HEIGHT := 854
-TARGET_SCREEN_WIDTH := 480
-
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := cm_urushi
 PRODUCT_DEVICE := urushi
