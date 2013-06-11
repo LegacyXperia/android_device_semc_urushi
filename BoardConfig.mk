ec981e603928adac38276d365e5372a800e2b7fc
Michael Bestas <mikeioannina@gmail.com>
2013-06-11 13:48:07 +0300

    Fix build
    Change-Id: Ibe72921072cfc2f5b7736bbf6a5ae84d28d05f41
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,10 +18,6 @@
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/semc/urushi/bluedroid
 
-# Boot Animation
-TARGET_SCREEN_HEIGHT := 854
-TARGET_SCREEN_WIDTH := 480
-
 # Kernel
 TARGET_KERNEL_CONFIG := nAa_urushi_defconfig
 
--- full_urushi.mk
+++ full_urushi.mk
@@ -18,6 +18,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from urushi device
 $(call inherit-product, device/semc/urushi/urushi.mk)
 
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 854
+TARGET_SCREEN_WIDTH := 480
+
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := full_urushi
 PRODUCT_DEVICE := urushi
