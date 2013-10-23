d22808b24663b8e63b59d48ba97db2434dfa0a17
Michael Bestas <mikeioannina@gmail.com>
2013-10-24 02:16:54 +0300

    Enable BLE
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>

--- bluedroid/bdroid_buildcfg.h
+++ bluedroid/bdroid_buildcfg.h
@@ -19,10 +19,15 @@
 #define _BDROID_BUILDCFG_H
 
 #define BTM_DEF_LOCAL_NAME   "Xperia Ray"
-#define BTHC_USERIAL_READ_MEM_SIZE (1034)
+
+#define BTHC_USERIAL_READ_MEM_SIZE 1034
 
 #define A2DP_HW_SYSFS_TUNER "/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq"
 #define A2DP_HW_SYSFS_TUNER_OFF "0"
 #define A2DP_HW_SYSFS_TUNER_ON "368640"
+
+#define BLE_INCLUDED TRUE
+#define BTA_GATT_INCLUDED TRUE
+#define SMP_INCLUDED TRUE
 
 #endif
