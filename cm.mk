65f66f583a0b0706ceef180ec4e39d88fb4c55e2
Michael Bestas <mikeioannina@gmail.com>
2013-12-17 02:29:46 +0200

    Add LX build tag

--- cm.mk
+++ cm.mk
@@ -39,3 +39,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := cm_urushi
 PRODUCT_DEVICE := urushi
+
+# Custom tag for unofficial builds
+TARGET_UNOFFICIAL_BUILD_ID := LegacyXperia
