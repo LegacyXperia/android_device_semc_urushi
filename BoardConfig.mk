dc2e11e53cb6be58f5be28e83aab5f94bced5433
Michael Bestas <mikeioannina@gmail.com>
2014-03-30 22:37:44 +0300

    urushi: Use new defconfig
    Change-Id: I3f48b69044d8f08abe10855941b762f70c9a040e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,7 +19,7 @@
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/semc/urushi/bluedroid
 
 # Kernel
-TARGET_KERNEL_CONFIG := nAa_urushi_defconfig
+TARGET_KERNEL_CONFIG := lx_urushi_defconfig
 
 # Sensors
 SOMC_CFG_SENSORS_ACCEL_BMA150_INPUT := yes
