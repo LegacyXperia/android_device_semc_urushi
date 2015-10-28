5dd8fda3381d70225125673fbdfca90e44e7dc88
Michael Bestas <mikeioannina@gmail.com>
2015-10-28 18:00:11 +0200

    LXify
    Change-Id: I531fdbe5e47506e85e7ee8ebe9cb18bd37d7176f

--- lx.mk
+++ lx.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2011-2013 The CyanogenMod Project
+# Copyright (C) 2015 LegacyXperia Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,8 +15,8 @@
 # Inherit device configuration
 $(call inherit-product, device/semc/urushi/full_urushi.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
+# Inherit some common LX stuff.
+$(call inherit-product, vendor/lx/config/common.mk)
 
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += \
@@ -26,8 +26,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="ST18i-user 4.0.4 4.1.B.0.587 tL1_3w test-keys"
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_urushi
+PRODUCT_NAME := lx_urushi
 PRODUCT_GMS_CLIENTID_BASE := android-sonyericsson
-
-# Custom tag for unofficial builds
-TARGET_UNOFFICIAL_BUILD_ID := LegacyXperia
